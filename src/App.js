41670dbcd248dddca404aa8729948045fcf4c10f
Lukasz <lukaszprzybylka@gmail.com>
2023-01-05 15:33:41 +0100

    Setting up MUI etc.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <>
+    <Button 
+    variant="contained" 
+    color='success' 
+    onClick={()=>console.log('button clicked')} >
+      Hello World
+    </Button>
+    </>
+  )
 }
-
 export default App;
